31848b7317ad5e93dd7baa479539cf82d58e17ba
SAIMADHUYAL <saimadhu.yal@gmail.com>
2021-04-25 21:56:59 -0500

    commit

--- Day5Assignment/src/app/app.module.ts
+++ Day5Assignment/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProductComponent,
-    ContactComponent
+    ContactComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
